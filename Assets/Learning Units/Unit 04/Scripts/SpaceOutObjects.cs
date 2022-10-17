319550d3cecef83a7ee172aa105c5d5eedcdcda7
Jason Bennett <jasoben@users.noreply.github.com>
2022-10-17 15:17:22 -0400

    Update SpaceOutObjects.cs

--- Assets/Learning Units/Unit 04/Scripts/SpaceOutObjects.cs
+++ Assets/Learning Units/Unit 04/Scripts/SpaceOutObjects.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class SpaceOutObjects : MonoBehaviour
 {
+    // DIRECTIONS: The goal of this Unit is to get the objects to space out evenly
+
     // Start is called before the first frame update
     void Start()
     {
+        // DIRECTION: Uncomment the lines below, then fix them...
+        //GameObject objectsToSpaceOut = GetComponent<MoveObjects>().ObjectsToMove;
         
+        //for (something; something something objectsToSpaceOut.Count; something)
+        //{
+        //    somethingsomething.transform.position = something * 2f;
+        //}
     }
 
     // Update is called once per frame
