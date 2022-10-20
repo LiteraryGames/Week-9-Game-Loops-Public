6ab856c15033b3ed6841e936cbf4ea306f1ff56d
Emily Kao <54684530+eckao@users.noreply.github.com>
2022-10-20 13:21:58 -0400

    attempting to do unit 5 (and failing)

--- Assets/Learning Units/Unit 05/Scripts/MoveBackAndForth.cs
+++ Assets/Learning Units/Unit 05/Scripts/MoveBackAndForth.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Numerics;
 using UnityEngine;
 
 public class MoveBackAndForth : MonoBehaviour
 {
     // DIRECTIONS: Get the objects to move back and forth.
     List<GameObject> ObjectsToMove { get; set; }
-    List<Vector3> startPositions;
-    List<Vector3> endPositions;
+    List<UnityEngine.Vector3> startPositions;
+    List<UnityEngine.Vector3> endPositions;
 
     float moveAmount = 0; // Don't change this line!
     float lerpAmount = 0; // Don't change this line!
@@ -17,25 +19,30 @@ public class MoveBackAndForth : MonoBehaviour
     {
         ObjectsToMove = GetComponent<MoveObjects>().ObjectsToMove; // Keep this line and don't change it.
         GetComponent<MoveObjects>().enabled = false; // Keep this line and don't change it.
-        
+
         //DIRECTIONS: Uncomment the lines below and fix them
-        //startPositions = new Something;
-        //endPositions = new Something;
-        //foreach(something in Something)
-        //{
-        //    someList.Add(something.transform.position); // one of these lists needs this kind of position...
-        //    thisOtherList.Add(something.transform.position + new Vector3(0,0,10f)); // another one needs another...
-        //}
+        startPositions = new List<UnityEngine.Vector3>();
+        endPositions = new List<UnityEngine.Vector3>();
+        foreach (GameObject obj in ObjectsToMove)
+        {
+            startPositions.Add(obj.transform.position); // one of these lists needs this kind of position...
+            endPositions.Add(obj.transform.position + new UnityEngine.Vector3(0, 0, 10f)); // another one needs another...
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         //DIRECTIONS: Uncomment the lines below and fix them
-        //for (something; something.Count something; something)
-        //{
-        //    someList[something].transform.position = Vector3.Lerp(someOtherList[something], aThirdList[something], lerpAmount);
-        //}
+       // UnityEngine.Debug.Log("num objs: " + ObjectsToMove.Count);
+
+        for (int i = 0; i < ObjectsToMove.Count ; i++)
+        {
+            ObjectsToMove[i].transform.position = UnityEngine.Vector3.Lerp(startPositions[i], endPositions[i], lerpAmount);
+            // UnityEngine.Debug.Log(startPositions[i] + " end: " + endPositions[i]);
+            //UnityEngine.Debug.Log(i);
+
+        }
 
         //Don't change anything below here!
         lerpAmount += moveAmount;
